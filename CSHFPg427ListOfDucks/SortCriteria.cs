899c79797ecb45b25b3b7248f29bf25ca6dbbb3b
Sean McKinney <seanamckinney8@gmail.com>
2022-06-20 13:19:11 -0500

    created a DuckComapere class and implemented IComparer<>.  Added enum to hold sort criteria. Created Compare method in DuckComparer to sort by SizeThenKind and KindThenSize.  Added needed code to Main to run both methods and write it to the console.

---
+++ CSHFPg427ListOfDucks/SortCriteria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSHFPg427ListOfDucks
+{
+    public enum SortCriteria
+    {
+        SizeThenKind,
+        KindThenSize,
+    }
+}
