c478e6b839e59eebcc822d352f45dc04afc0879b
Sean McKinney <seanamckinney8@gmail.com>
2022-06-20 12:08:39 -0500

    Add project files.

---
+++ CSHFPg427ListOfDucks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHFPg427ListOfDucks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
